b0f4d5ec8494e64fd1ea889734ce8d9e8070746b
Doug Hauenstein <doug.hauenstein@gmail.com>
2022-12-14 13:15:27 -0500

    added FM as data source

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
 import c3 from "c3";
 
-const data = [
-    {
-      "month": "Jan",
-      "Apples": 327,
-      "Peaches": 437,
-      "Pears": 111
-    },
-    {
-      "month": "Feb",
-      "Apples": 555,
-      "Peaches": 454,
-      "Pears": 222
-    },
-    {
-      "month": "Mar",
-      "Apples": 242,
-      "Peaches": 864,
-      "Pears": 343
-    },
-    {
-      "month": "Apr",
-      "Apples": 222,
-      "Peaches": 454,
-      "Pears": 434
-    }
-];
+window.loadChart = function (json) {
+console.log("loadChart", json);
+const obj = JSON.parse(json);
+console.log(obj);
+const data = obj.data;
+const chartType = obj.chartType;
+console.log(chartType);
+
 
 const options =  { 
   size: {height : 360, width  : 420},
@@ -39,13 +21,15 @@ const options =  {
 
 data: {
   labels : true,
-  type   : 'bar',
+  type   : chartType,
   json   :  data,
   keys: {
         x: "month", 
-        value: ['Apples', 'Peaches', 'Pears']
+        value: ['Peaches' ,'Pears', 'Apples' ]
       },
    },
 };
 // chart variable calling options variable
-const chart = c3.generate(options);
+const chart = c3.generate(options);  
+
+};

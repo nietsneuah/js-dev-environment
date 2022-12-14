9fd4707982e2804bc9720ce7a1e38dd6ec377185
Doug Hauenstein <doug.hauenstein@gmail.com>
2022-12-14 11:36:15 -0500

    Chart working with fixed data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+const data = [
+    {
+      "month": "Jan",
+      "Apples": 327,
+      "Peaches": 437,
+      "Pears": 111
+    },
+    {
+      "month": "Feb",
+      "Apples": 555,
+      "Peaches": 454,
+      "Pears": 222
+    },
+    {
+      "month": "Mar",
+      "Apples": 242,
+      "Peaches": 864,
+      "Pears": 343
+    },
+    {
+      "month": "Apr",
+      "Apples": 222,
+      "Peaches": 454,
+      "Pears": 434
+    }
+];
+
+const options =  { 
+  size: {height : 360, width  : 420},
+  bindto: "#chart",
+  axis: {
+    x: {type: "category" },
+    y: {},
+  }, 
+  
+
+
+data: {
+  labels : true,
+  type   : 'bar',
+  json   :  data,
+  keys: {
+        x: "month", 
+        value: ['Apples', 'Peaches', 'Pears']
+      },
+   },
 };
-
-//
+// chart variable calling options variable
+const chart = c3.generate(options);

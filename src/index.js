21bfb8ae731e108e32941bf6554eac7979b6a720
Doug Hauenstein <doug.hauenstein@gmail.com>
2022-12-16 14:25:03 -0500

    Chart - interactive with FM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import c3 from "c3";
+const months = [
+"Jan",
+"Feb",
+"Mar",
+"Apr",
+"May",
+"Jun",
+"Jul",
+"Aug",
+"Sep",
+"Oct",
+"Nov",
+"Dec",
+];
 
 window.loadChart = function (json) {
 console.log("loadChart", json);
-//create an variable that contains the json passed by the FM script named loadChart
+//create a variable that contains the json passed by the FM script named loadChart
 const obj = JSON.parse(json);
 console.log(obj);
 // create a variable that contains the value from element "data" an Array from obj(json) 
@@ -26,6 +40,22 @@ const options =  {
 
 
 data: {
+  onclick: function(d){
+    console.log("data",d);
+    // const index = d.index;
+    // const name = d.name;
+    // const value = d.value;
+    // optional way to define multiple variables that are values of an object
+    // is called "deconstruction"
+    const {index, name, value} = (d);
+    console.log("Index", index);
+    const month = months[index];
+    console.log("Month",month);
+    // adding Month: before the const name changes the value of the variable returned to the DOM
+    const newObj = {month, name, value};
+    console.log("new object", newObj);
+    FileMaker.PerformScript("On Chart Click",JSON.stringify (newObj));
+  },
   labels : true,
   type   : chartType,
   json   :  data,
@@ -35,7 +65,28 @@ data: {
       },
    },
 };
+
+
 // chart variable calling options variable
 const chart = c3.generate(options);  
 
+window.transformChart = function (type) {
+  chart.transform(type);
 };
+window.loadData = function (json) {
+  const obj = JSON.parse(json);
+console.log(obj);
+// create a variable that contains the value from element "data" an Array from obj(json) 
+const data = obj.data;
+console.log(data);
+chart.load({
+  json: data,
+  keys: {
+    x: "month", 
+    value: ['Bananas']
+  },
+})
+
+};
+};
+
